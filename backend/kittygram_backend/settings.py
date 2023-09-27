0aa10ed5d06a889663c06f0e9262fe7b7db8688b
mazilinsp <admin@example.com>
2023-09-27 12:06:00 +0000

    выключен дебаг режим

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['158.160.81.84', '127.0.0.1', 'localhost', 'kittygrm.ddns.net']
 
